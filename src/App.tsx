47cd09c1ac972a2d0b3735df3eb19379f163865c
deivis herrera <herrera.15cortes@gmail.com>
2025-05-02 01:14:23 -0500

    Initialize project with Vite + React + TypeScript + TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="container mx-auto mt-20">
+      <h1 className="text-4xl font-bold text-blue-500">
+      ¡Hola Tailwind!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
